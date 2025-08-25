846e171e9a395e8bae41cc93068e88da28d2ead8
Abdelrahmanyehia <https://github.com/Abdelrahmanyehia98/Amit_Diploma.git>
2025-08-25 08:07:18 +0300

    added the modules and db

---
+++ src/Modules/Users/user.controller.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import * as userServices from "./Services/user.service.js";
+import { authenticationMiddleware } from "../../Middlewares/authentication.middleware.js";
+const router = Router();
+
+
+router.post("/signup", userServices.signUpService);
+router.post("/signin", userServices.signinService);
+router.post("/logout",authenticationMiddleware, userServices.LogoutService);
+router.put("/update", authenticationMiddleware , userServices.updateAccountService);
+router.delete("/delete", authenticationMiddleware ,userServices.deleteAccountService);
+router.get("/list", userServices.listUsersService);
+router.put("/confirm", userServices.confirmEmailService)
+
+export default router;
+
+
